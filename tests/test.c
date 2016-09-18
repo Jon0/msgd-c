fd4885066bac6eaac34e581511d7c3b3718817cb
Jon0 <Jono4728@gmail.com>
2016-09-18 20:57:48 +1200

    sub command

--- tests/test.c
+++ tests/test.c
@@ -4,22 +4,44 @@
 #include <libmsg/msgd.h>
 
 
+int read_int() {
+    char *end;
+    char inbuf [32];
+    fgets(inbuf, sizeof(inbuf), stdin);
+    return strtol(inbuf, &end, sizeof(inbuf));
+}
+
+
 void read_command(struct msg_client_state *ns) {
     struct msg_node_set nds;
     char inbuf [256];
+    int nodebuf;
 
     // read input
     // either connect or create a nodes
+    printf("enter cmd (init, add or sub)\n");
     scanf("%s", inbuf);
+    if (strcmp(inbuf, "init") == 0) {
+        printf("enter process name\n");
+        scanf("%s", inbuf);
+        printf("initalising %s\n", inbuf);
+        msg_init_proc(ns, inbuf, 0);
 
-    //
-    printf("adding %s\n", inbuf);
-    msg_init_proc(ns, inbuf, 0);
-
-    // get available nodes
-    // block until result is recieved
-    printf("requesting available\n");
-    msg_available(ns, &nds);
+        // get available nodes
+        // block until result is recieved
+        printf("requesting available\n");
+        msg_available(ns, &nds);
+    }
+    else if (strcmp(inbuf, "add") == 0) {
+        printf("enter node name\n");
+        scanf("%s", inbuf);
+        printf("adding %s\n", inbuf);
+    }
+    else if (strcmp(inbuf, "sub") == 0) {
+        printf("enter node id\n");
+        nodebuf = read_int();
+        printf("subscribing %d\n", nodebuf);
+    }
 }
 
 
