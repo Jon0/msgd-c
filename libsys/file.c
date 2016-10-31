4c6b329fa20c88b665a0902781839a96a6e5618b
Jon0 <Jono4728@gmail.com>
2016-11-01 00:50:01 +1300

    mount function

--- libsys/file.c
+++ libsys/file.c
@@ -1,1 +1,6 @@
 #include "file.h"
+
+
+int ep_mount_path(struct ep_tree *t, const char *path) {
+
+}
--- libsys/file.h
+++ libsys/file.h
@@ -1,8 +1,15 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <libutil/node.h>
+#include <libutil/tree.h>
 
 
+/*
+ * add files or directories to tree
+ * return node id
+ */
+int ep_mount_path(struct ep_tree *t, const char *path);
 
 
 #endif
