df02226fe58ab90b5dc64b0bf50d58d451d4b3ee
Jon0 <Jono4728@gmail.com>
2016-11-01 12:08:25 +1300

    note params

--- libsys/file.c
+++ libsys/file.c
@@ -2,5 +2,7 @@
 
 
 int ep_mount_path(struct ep_tree *t, const char *path) {
-
+    // require extra parameters for msgd node_to_sub
+    // subid and type labels
+    // default to "mount/files"
 }
