39ed66eb49dbc2bbb0f31801c014493a0497ff68
Jon0 <Jono4728@gmail.com>
2016-12-18 00:21:50 +1300

    network types

---
+++ libutil/key.h
@@ -0,0 +1,13 @@
+#ifndef LIBUTIL_KEY_H
+#define LIBUTIL_KEY_H
+
+
+/*
+ * a set of permissions given to a key
+ */
+struct msgu_key {
+
+};
+
+
+#endif
---
+++ libutil/network.h
@@ -0,0 +1,13 @@
+#ifndef LIBUTIL_NETWORK_H
+#define LIBUTIL_NETWORK_H
+
+
+/*
+ * a set of hosts
+ */
+struct msgu_network {
+    int network_id;
+};
+
+
+#endif
