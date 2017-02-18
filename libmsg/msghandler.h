91351fca8d29e07802e4fde4aaafdedd38a983b6
Jon0 <Jono4728@gmail.com>
2017-02-18 22:33:52 +1300

    interface types

---
+++ libmsg/msghandler.h
@@ -0,0 +1,23 @@
+#ifndef LIBMSG_MSGHANDLER_H
+#define LIBMSG_MSGHANDLER_H
+
+#include "server.h"
+
+
+/*
+ * events from loopback interface
+ */
+struct msg_loopback {
+
+};
+
+
+/*
+ * events from socket acceptors
+ */
+struct msg_acceptor {
+
+};
+
+
+#endif
