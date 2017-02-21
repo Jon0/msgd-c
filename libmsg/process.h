0f7697d0c664dc76fef7f1e19de2bf56814f525a
Jon0 <Jono4728@gmail.com>
2017-02-21 21:01:00 +1300

    move server elements

--- libmsg/process.h
+++ libmsg/process.h
@@ -1,14 +1,30 @@
 #ifndef LIBMSG_PROCESS_H
 #define LIBMSG_PROCESS_H
 
+#include <libsys/fuse.h>
+#include <libsys/network.h>
+#include <libsys/table.h>
+#include <libsys/thread.h>
+
+
 #include "server.h"
 
 
 struct msg_process {
     struct msg_server state;
 
+    // event handling
+    struct msgs_event_map emap;
+    struct msgs_table     tb;
+
+    // listening sockets
+    int                  local_acc_id;
+    struct msgs_acceptor local_acc;
+    int                  remote_acc_id;
+    struct msgs_acceptor remote_acc;
 
 
+    struct msgs_fuse_files *fuse;
 };
 
 
